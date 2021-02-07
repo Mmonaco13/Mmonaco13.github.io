25d4acc1a3810f04b613c3bb170e8d9b2420a299
Mmonaco13 <mmonaco1@stevens.edu>
2021-02-07 23:34:21 +0000

    Fixed Headshot and Added Random Color

---
+++ js/main.js
@@ -0,0 +1,36 @@
+$( document ).ready(function() {
+  let root = document.documentElement;
+
+  let color = Math.floor(Math.random() * 10);
+  switch(color) {
+    case 0:
+      root.style.setProperty('--accent', '#922B21'); // red
+      break;
+    case 1:
+      root.style.setProperty('--accent', '#9B59B6'); // purple
+      break;
+    case 3:
+      root.style.setProperty('--accent', '#1F618D'); // blue
+      break;
+    case 4:
+      root.style.setProperty('--accent', '#148F77'); // sea blue
+      break;
+    case 5:
+      root.style.setProperty('--accent', '#1E8449'); // green
+      break;      
+    case 6:
+      root.style.setProperty('--accent', '#B7950B'); // yellow
+      break; 
+    case 7:
+      root.style.setProperty('--accent', '#AF601A'); // orange
+      break; 
+    case 8:
+      root.style.setProperty('--accent', '#FA8072'); // salmon
+      break; 
+    case 9:
+      root.style.setProperty('--accent', '#808000'); // olive
+      break; 
+    default:
+      root.style.setProperty('--accent', '#FA8072');
+  }
+});
